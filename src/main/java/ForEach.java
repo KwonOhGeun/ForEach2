cfc0f5b2271bd2a8ec6b03c1c49501dd928c99ef
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-05-12 23:50:36 +0900

    2022.05.12 ForEach

--- src/main/java/ForEach.java
+++ src/main/java/ForEach.java
@@ -1,9 +1,9 @@
 public class ForEach {
     public static void main(String[] args) {
-        int [] n = {1,2,3,4,5};
+        int[] n = {1, 2, 3, 4, 5};
         int sum = 0;
 
-        for(int num : n){
+        for (int num : n) {
             sum += num;
         }
         System.out.println(sum);
