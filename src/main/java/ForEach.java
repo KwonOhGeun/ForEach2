b70cb8891f03250f91d4e8657c4fc7b5142421a2
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-05-12 23:50:17 +0900

    Initial commit

---
+++ src/main/java/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int [] n = {1,2,3,4,5};
+        int sum = 0;
+
+        for(int num : n){
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
